8d4065d9de15114806a30ee48bb199731498ff4a
Luiz Fernando Aguilar Althoff <luizfernando.althoff@gmail.com>
2026-06-02 15:51:47 -0300

    combinador linear, invocação de actFunc, softmax actfunc, cross entropy categorica, iteração básica do neurônio, swish actfunc

--- api/main.py
+++ api/main.py
@@ -3,8 +3,7 @@ import numpy as np
 class Rede:
     def __init__(self, dict_hiperparameters):
         self.network_matrix = np.empty(0, dtype=object)
-        self.activation_functions = {
-            "binary_step": self.binaryStepActFunction,
+        self.activation_functions_dict = {
             "linear": self.linearActFunction,
             "sigmoid": self.sigmoidActFunction,
             "tanh": self.tanhActFunction,
@@ -58,26 +57,38 @@ class Rede:
     def derivativeELU(self):
         return
     
-    def swishActFunction(self):
-        return
+    def swishActFunction(self, x_input_value):
+        y_output_value = x_input_value * (1 / (1 + np.exp(-x_input_value)))
+        return y_output_value
     
     def derivativeSwish(self):
         return
     
-    def softmaxActFunction(self):
-        return
+    def softmaxActFunction(self, x_input_vector):
+        exp_values_vector = np.exp(x_input_vector)
+        exp_values_vector = exp_values_vector / np.sum(exp_values_vector)
+        return exp_values_vector
     
-    def derivativeSoftmax(self):
-        return
-    
+'''
+    eu fiz a derivada da softmax, 
+    mas pelo que li na web essa função n importa muito 
+    pois já está incorporada no cálculo da cross entropy categorica
+
+    def derivativeSoftmax(self, softmax_vector): 
+        sigma = np.array(softmax_vector)
+        jacobian_matrix = np.diag(sigma) - np.outer(sigma, sigma)
+        return jacobian_matrix
+'''
+
     def squareErrorCostFunction(self):
         return
     
     def crossEntropyCostFunction(self):
         return
     
-    def categoricCrossEntropyCostFunction(self):
-        return
+    def categoricCrossEntropyCostFunction(self, y_predicted_vector, y_true_vector):
+        loss_value = -np.sum(y_true_vector * np.log(y_predicted_vector))
+        return loss_value
     
     def feedFoward(self):
         return
@@ -88,9 +99,27 @@ class Rede:
     def gradientDescent(self):
         return
     
-    def linearCombinerNeuron(self, x_inputs_vector, w_weight_matrix):
-        return
+    def linearCombinerNeuron(self, k_number_neurons_in_layer, w_weight_vector, x_inputs_vector):
+        v_sum_value = 0
+        for i in range(k_number_neurons_in_layer):
+            v_sum_value  += w_weight_vector[i]*x_inputs_vector[i]
+        return v_sum_value 
     
-    def useActFunction(self, act_function_name) # a ideia é que receba o nome da função como string, que será usada como key do dicionário. Python permite invocar métodos deste jeito
-        # y = self.activation_functions[act_function_name](v)
-        return
+    '''
+    Aqui a gente está passando o nome da função a ser chamada (ex. "relu"), que será usada como chave do dicionário de funções
+    que armazena uma referência ao método correspondente. Então quando a estrutura de dicionário chamar essa referência, os parênteses
+    do lado que armazenam o valor de input serão passados para o método 'invocado'.
+
+    ex:
+    no código ficará                --> self.activation_functions_dict["relu"](-0.5) 
+    mas isso vai se transformar em  --> self.reluActFunction(x)
+    e retornará                     --> 0
+    '''
+    def useActFunction(self, function_name_string, v_sum_value):
+        y_output_value = self.activation_functions_dict[function_name_string](v_sum_value)
+        return y_output_value
+    
+    def neuronIteration(self, k_number_neurons_in_layer, w_weight_vector, x_inputs_vector, function_name_string):
+        v_sum_value = self.linearCombinerNeuron(k_number_neurons_in_layer, w_weight_vector, x_inputs_vector)
+        y_output_value = self.useActFunction(function_name_string, v_sum_value)
+        return y_output_value
